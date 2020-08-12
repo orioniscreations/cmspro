c934c909e6396f36d27e0ffce36b858108671a69
Aaron <rysrps@gmail.com>
2020-08-12 18:13:32 -0300

    i cry

--- site/js/bulbaJS.js
+++ site/js/bulbaJS.js
@@ -72,4 +72,160 @@
 
 //starting values 
 
+const IMAGES = {
+    default: "bulbaGif.gif",
+    happy: "bulbaHappy.gif",
+    sad: "bulbaSad.gif",
+    sleepy: "bulbaSleepy.gif"
+}
+
+
+var game = new Vue({
+    el: "#bodyCont",
+    data: {
+        play: 1000,
+        health: 1000,
+        food: 2000,
+        grooming: 3000,
+        happiness: 100,
+        sleepy: 0,
+        currentImage: IMAGES.default
+    },
+
+
+})
+
+// // ********************************
+
+function lessPlay() {
+    var minusPlay = 300;
+    var timer = null
+
+    if (game.play === -1) { return; }
+    //   testing
+    // console.log("play: " + game.play);
+
+    setTimeout(lessPlay, minusPlay);
+
+    minusPlay -= 100;
+    game.play--;
+};
+
+lessPlay();
+
+function lessFood() {
+    var minusFood = 1000;
+    var timer = null;
+
+    if (game.food === -1) { return; }
+    //   testing
+    // console.log("food: " + game.food);
+
+    setTimeout(lessFood, minusFood);
+
+    minusFood -= 100;
+    game.food--;
+};
+
+lessFood();
+
+function lessGrooming() {
+    var minusGroom = 3000
+    var timer = null;
+
+    if (game.grooming === -1) { return; }
+    //testing
+    // console.log("grooming: " + game.grooming);
+
+    setTimeout(lessGrooming, minusGroom);
+
+
+    minusGroom -= 5000;
+    game.grooming--;
+};
+
+lessGrooming();
+
+function lessHealth() {
+    var minusHealth = 300
+    var timer = null;
+
+    if (game.health === -1) { return; }
+
+
+    setTimeout(lessHealth, minusHealth);
+
+
+    minusHealth -= 10;
+    game.health--;
+}
+
+function highHealth() {
+
+    if (game.play >= 1000 && game.food >= 2000 && game.grooming >= 3000) {
+        game.currentImage = IMAGES.happy;
+    } else if
+        (game.play <= 300 && game.play >= 2 || game.food <= 500 && game.food >= 2 || game.grooming <= 750 && game.grooming <= 2) {
+        game.currentImage = IMAGES.sad;
+        lessHealth();
+    } else if
+        (game.play <= 1 || game.food <= 1 || game.grooming <= 1) {
+        game.currentImage = IMAGES.sleepy;
+        sleepyTime();
+    } else { game.currentImage = IMAGES.default }
+
+    setTimeout(highHealth, 500)
+}
+
+highHealth();
+
+function sleepyTime() {
+    if (game.play <= 1 || game.food <= 1 || game.grooming <= 1) {
+
+    }
+}
+
+// function lessHealth() {
+//     if (game.play <= 300 || game.food <= 500 || game.grooming <= 750) {
+//         var minusHealth = 5000;
+//         var timer = null;
+
+//         if (game.health === -1) { return; }
+
+
+//         setTimeout(lessHealth, minusHealth);
+
+//         minusHealth -= 100;
+//         game.health--;
+//     }
+
+// }
+
+// lessHealth();
+
+// // ********************************
+
+function morePlay() {
+    game.play += 100
+    console.log("play: " + game.play);
+}
+
+function moreHealth() {
+    game.health += 1000
+    console.log("health: " + game.health);
+
+    if (game.play === 300 || game.food === 300 || game.grooming === 300) {
+        game.happiness--;
+    } return game.happiness;
+}
+
+function moreFood() {
+    game.food += 1000
+    console.log("food: " + game.food);
+}
+
+function moreGrooming() {
+    game.grooming += 1000
+    console.log("groom: " + game.grooming);
+}
 
